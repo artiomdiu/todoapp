f5d769a7a9489688b42485073dbfe50c3a75602f
AD <artiomd@gmail.com>
2021-10-26 13:30:39 +0300

    web page loads, but there are some problems with r/w to DB

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://udacitystudios@localhost:5432/todoapp'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres586@localhost:5432/todoapp'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-
 migrate = Migrate(app, db)
 
 
@@ -25,10 +25,12 @@ def create_todo():
     error = False
     body = {}
     try:
-        description = request.form.get_json()['description']
-        todo = Todo(description=description)
+        description = request.get_json()['description']
+        todo = Todo(description=description, comleted=False)
         db.session.add(todo)
         db.session.commit()
+        body['id'] = todo.id
+        body['completed'] = todo.completed
         body['description'] = todo.description
     except:
         error = True
@@ -42,14 +44,30 @@ def create_todo():
         return jsonify(body)
 
 
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+def set_completed_todo(todo_id):
+    try:
+        completed = request.get_json()['completed']
+        print('completed', completed)
+        todo = Todo.query.get(todo_id)
+        todo.completed = completed
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return redirect(url_for('index'))
+
+
 @app.route('/')
 def index():
-    return render_template('index.html', data=Todo.query.all())
+    return render_template('index.html', data=Todo.query.order_by('id').all())
 
 
-if __name__ == '__main__':
-    app.run(
-        degug=True,
-        host='127.0.0.1',
-        port=5432
-    )
+# if __name__ == '__main__':
+#     app.run(
+#         debug=True,
+#         # host='0.0.0.0',
+#         host='127.0.0.1',
+#         port=5000
+#     )
